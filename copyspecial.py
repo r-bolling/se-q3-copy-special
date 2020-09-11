249d879aa65036de51d14dcc7831b86750faf2eb
Robert Bolling <robboles1014@gmail.com>
2020-09-10 20:09:19 -0400

    Completed get_special_paths function

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "r-bolling with help from Kenzie Academy"
 
 import re
 import os
@@ -20,15 +20,26 @@ import argparse
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
     # your code here
-    return
+    special_files = []
+    dir_list = os.listdir(dirname)
+    special_file_pattern = re.compile(r'.*__\w+__.*')
+    for directory in dir_list:
+        special_files.extend(special_file_pattern.findall(directory))
+    # special_files = special_file_pattern.findall(dir_list)
+    # print(special_files)
+    return [os.path.abspath(x) for x in special_files]
 
 
 def copy_to(path_list, dest_dir):
+    '''Given the --todir arg, a target dir and a destination dir,
+    copy the files in the destination dir to the target dir'''
     # your code here
     return
 
 
 def zip_to(path_list, dest_zip):
+    '''Given the --tozip arg, a target dir and a destination zip,
+    zip the files in the target dir to the destination zip'''
     # your code here
     return
 
@@ -40,8 +51,13 @@ def main(args):
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
     # TODO: add one more argument definition to parse the 'from_dir' argument
+    parser.add_argument('from_dir', help='target directory')
     ns = parser.parse_args(args)
 
+    if ns.from_dir:
+        special_files = get_special_paths(ns.from_dir)
+        for special_file in special_files:
+            print(special_file, end='\n')
     # TODO: you must write your own code to get the command line args.
     # Read the docs and examples for the argparse module about how to do this.
 
